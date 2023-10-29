761ef8b669ecfc132bda1ffde319893109485ab8
Jorge Maicon <109354672+JorgeMaiconFaria@users.noreply.github.com>
2023-10-29 17:31:11 -0300

    Adicionado imagens de fundo e script do menu

--- script/script.js
+++ script/script.js
@@ -2,27 +2,25 @@ let buttonLifeIncrease1 = document.getElementById("lifeIncrease1");
 let buttonLifeIncrease5 = document.getElementById("lifeIncrease5");
 let buttonLifeDecrease1 = document.getElementById("lifeDecrease1");
 let buttonLifeDecrease5 = document.getElementById("lifeDecrease5");
+let menuBTN = document.getElementById("menuBTN");
 let lifeSpan = document.getElementById("lifeSpan")
-
 let count = 20
 
-function lifeIncrease() {
-    lifeSpan.innerHTML = count += 1;
+function lifeIncrease(value) {
+    lifeSpan.innerHTML = count += value;
 }
 
-function lifeIncrease5() {
-    lifeSpan.innerHTML = count += 5;
+function lifeDecrease(value) {
+    lifeSpan.innerHTML = count -= value;
 }
 
-function lifeDecrease1() {
-    lifeSpan.innerHTML = count -= 1;
+function showMenu() {
+    let menu = document.getElementById("menu");
+    menu.style.display == "block" ? menu.style.display = "none" : menu.style.display = "block"
 }
 
-function lifeDecrease5() {
-    lifeSpan.innerHTML = count -= 5;
-}
-
-buttonLifeIncrease1.addEventListener("click", lifeIncrease)
-buttonLifeIncrease5.addEventListener("click", lifeIncrease5)
-buttonLifeDecrease1.addEventListener("click", lifeDecrease1)
-buttonLifeDecrease5.addEventListener("click", lifeDecrease5)
+buttonLifeIncrease1.addEventListener("click", () => lifeIncrease(1));
+buttonLifeIncrease5.addEventListener("click", () => lifeIncrease(5));
+buttonLifeDecrease1.addEventListener("click", () => lifeDecrease(1));
+buttonLifeDecrease5.addEventListener("click", () => lifeDecrease(5));
+menuBTN.addEventListener("click", showMenu)
