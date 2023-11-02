eac3be4bb1ac061f3f4f21df6e665c5740780e94
Jorge Maicon <109354672+JorgeMaiconFaria@users.noreply.github.com>
2023-11-02 11:40:04 -0300

    Adicionado função Monarca, troca de imagem de fundo e div para poison de energy.

--- script/script.js
+++ script/script.js
@@ -2,19 +2,47 @@ let buttonLifeIncrease1 = document.getElementById("lifeIncrease1");
 let buttonLifeIncrease5 = document.getElementById("lifeIncrease5");
 let buttonLifeDecrease1 = document.getElementById("lifeDecrease1");
 let buttonLifeDecrease5 = document.getElementById("lifeDecrease5");
+let btnWhite = document.getElementById("branco");
+let btnBlue = document.getElementById("azul");
+let btnBlack = document.getElementById("preto");
+let btnRed = document.getElementById("vermelho");
+let btnGreen = document.getElementById("verde");
+let btnAzhorus = document.getElementById("azhorus");
+let btnDimir = document.getElementById("dimir");
+let btnRakdos = document.getElementById("rakdos");
+let btnGruul = document.getElementById("gruul");
+let btnSelesnya = document.getElementById("selesnya");
+let btnOrzhov = document.getElementById("orzhov");
+let btnBoros = document.getElementById("boros");
+let btnIzzit = document.getElementById("izzit");
+let btnSimic = document.getElementById("simic");
+let btnGolgari = document.getElementById("golgari");
+let crownBtn = document.getElementById("crown");
+let btnMoon = document.getElementById("moon");
+let btnEnergy = document.getElementById("energy");
+let btnPoison = document.getElementById("poison");
+let monarchBtn = document.getElementById("monarchImg");
 let menuBTN = document.getElementById("menuBTN");
 let lifeSpan = document.getElementById("lifeSpan")
 let count = 20
 
 function lifeIncrease(value) {
-    lifeSpan.innerHTML = count += value;
+    if(count < 1000) {
+        lifeSpan.innerHTML = count += value;
+    }
 }
 
 function lifeDecrease(value) {
-    lifeSpan.innerHTML = count -= value;
+    if(count > -100) {
+        lifeSpan.innerHTML = count -= value;
+    }
 }
 
-function show() {
+function showMonarch() {
+    !monarchBtn.classList.contains("show") ? monarchBtn.classList.add("show") : monarchBtn.classList.remove("show")
+}
+
+function showMenu() {
     let menu = document.getElementById("menu");
     if(!menu.classList.contains("show")) {
         menu.style.display = "block"
@@ -23,12 +51,39 @@ function show() {
     } else {
         menu.classList.remove("show")
         setTimeout(function() {menu.style.display = "none"
-    }, 1500)
+    }, 100)
     }
 }
 
-buttonLifeIncrease1.addEventListener("click", () => lifeIncrease(1));
-buttonLifeIncrease5.addEventListener("click", () => lifeIncrease(5));
-buttonLifeDecrease1.addEventListener("click", () => lifeDecrease(1));
-buttonLifeDecrease5.addEventListener("click", () => lifeDecrease(5));
-menuBTN.addEventListener("click", show)
+function changeBackground(url, color) {
+    document.body.style.backgroundImage = url
+    lifeSpan.style.color = color
+}
+
+function activeMonarch() {
+    monarchBtn.classList.contains("show") && monarchBtn.style.opacity == 1 ? 
+    monarchBtn.style.opacity = 0.2 : monarchBtn.style.opacity = 1
+}
+
+buttonLifeIncrease1.addEventListener("mousedown", () => lifeIncrease(1));
+buttonLifeIncrease5.addEventListener("mousedown", () => lifeIncrease(5));
+buttonLifeDecrease1.addEventListener("mousedown", () => lifeDecrease(1));
+buttonLifeDecrease5.addEventListener("mousedown", () => lifeDecrease(5));
+menuBTN.addEventListener("click", showMenu);
+crownBtn.addEventListener("mousedown", showMonarch);
+monarchBtn.addEventListener("mousedown", activeMonarch)
+btnWhite.addEventListener("mousedown", () => changeBackground("url(../img/plains.jpg)", "black"));
+btnBlue.addEventListener("mousedown", () => changeBackground("url(../img/island.jpg)", "black"));
+btnBlack.addEventListener("mousedown", () => changeBackground("url(../img/swamp.jpg)", "black"));
+btnRed.addEventListener("mousedown", () => changeBackground("url(../img/mountain.jpg)", "black"));
+btnGreen.addEventListener("mousedown", () => changeBackground("url(../img/forest.jpg)", "black"));
+btnAzhorus.addEventListener("mousedown", () => changeBackground("url(../img/azhorus.jpg)", "black"));
+btnBoros.addEventListener("mousedown", () => changeBackground("url(../img/boros.jpg)", "black"));
+btnDimir.addEventListener("mousedown", () => changeBackground("url(../img/dimir.jpg)", "black"));
+btnGolgari.addEventListener("mousedown", () => changeBackground("url(../img/golgari2.jpg)", "lightgreen"));
+btnGruul.addEventListener("mousedown", () => changeBackground("url(../img/gruul.jpg)", "black"));
+btnIzzit.addEventListener("mousedown", () => changeBackground("url(../img/izzit.jpg)", "black"));
+btnSimic.addEventListener("mousedown", () => changeBackground("url(../img/simic.jpg)", "black"));
+btnSelesnya.addEventListener("mousedown", () => changeBackground("url(../img/selesnya.jpg)", "lightgreen"));
+btnRakdos.addEventListener("mousedown", () => changeBackground("url(../img/rakdos.jpg)", "black"));
+btnOrzhov.addEventListener("mousedown", () => changeBackground("url(../img/orzhov1.jpg)", "black"));
